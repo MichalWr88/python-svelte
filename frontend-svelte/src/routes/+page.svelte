8b26abd4d73b123f17db97154699fcd02e3815c0
mmaleszewski <mmaleszewski@trans.eu>
2024-10-31 12:15:10 +0100

    Initialize Svelte frontend with Tailwind CSS, ESLint, Prettier, and Vite configuration

---
+++ frontend-svelte/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="border-2 p-2 text-center shadow-xl">
+	<h1 class="text-blue-950  first-letter:bg-slate-50 first-letter:rounded-lg first-letter:p-1">Welcome to Svelte App</h1>
+
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
